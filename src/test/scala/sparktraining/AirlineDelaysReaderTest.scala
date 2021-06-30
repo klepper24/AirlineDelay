18011ab48c577015453b75a5f8ebcf5982c697ba
mklep <m.j.klepacki@gmail.com>
2021-06-30 14:37:24 +0200

    reader test

--- src/test/scala/sparktraining/AirlineDelaysReaderTest.scala
+++ src/test/scala/sparktraining/AirlineDelaysReaderTest.scala
@@ -7,11 +7,12 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import sparktraining.model.AirLineDelay
 
+
 class AirlineDelaysReaderTest extends AnyWordSpec with Matchers with DatasetSuiteBase {
 
     private implicit val context: SparkContext = sc
     import spark.implicits._
-    import sqlContext.implicits._
+    //import sqlContext.implicits._ zakomentowalem, bo ta linijka psula kompilacje
 
     "AirlineDelaysReader" should {
         "read delays csv file into dataset" in {
@@ -22,8 +23,8 @@ class AirlineDelaysReaderTest extends AnyWordSpec with Matchers with DatasetSuit
             val delays: Dataset[AirLineDelay] = AirlineDelaysReader.read(path)
 
             // then
-            val encoder = org.apache.spark.sql.Encoders.product[AirLineDelay]
-            val expectedDs: Dataset[AirLineDelay] = sc.parallelize(List("", "", 3)).as(encoder)
+            val ds = sc.parallelize(Seq(AirLineDelay("DCA", "EWR", 4.0), AirLineDelay("EWR", "IAD", -8.0), AirLineDelay("EWR", "DCA", -9.0), AirLineDelay("DCA", "EWR", -12.0), AirLineDelay("IAD", "EWR", -38.0)))
+            val expectedDs = ds.toDS()
             assertDatasetEquals(delays, expectedDs) // equal
         }
     }
